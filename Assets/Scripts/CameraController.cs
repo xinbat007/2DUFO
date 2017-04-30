1da0de5b446006cf5795ce4cff6b828766ddfda9
Pham Quang Thien <thienpq88@gmail.com>
2017-04-30 11:47:43 +0700

    1st done

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	
+	public Transform player;       //Public variable to store a reference to the player game object
+	private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+	// Use this for initialization
+	void Start () 
+	{
+		offset = transform.position - player.position;
+	}
+
+	void LateUpdate () 
+	{
+		transform.position = player.position + offset;
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 10.0f;
+	public Text countText;
+
+	private Rigidbody2D rb2d;
+	private int count;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		SetStringText ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed, ForceMode2D.Force);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetStringText ();
+		}
+	}
+
+	void SetStringText()
+	{
+		countText.text = "Count: " + count;
+	}
+}
